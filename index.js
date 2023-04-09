125299b915b9c88090b8dc70bd9c2154641dfc2a
Scott Schwab <scottschwab86@gmail.com>
2023-04-09 11:19:57 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(arrOfNames, name) {
+  let matchingNames = arrOfNames.filter((driverName) => driverName.toLowerCase() === name.toLowerCase());
+  return matchingNames;
+}
+
+function fuzzyMatch(arrOfNames, prefix) {
+  let prefixLength = prefix.length;
+  let matchingNames = arrOfNames.filter((driverName) => {
+    return driverName.slice(0, prefixLength) === prefix;
+  });
+  return matchingNames;
+}
+
+function matchName(arrOfDriverObjs, name) {
+  let matchingObjs = arrOfDriverObjs.filter((driverObj) => {
+    return driverObj.name === name;
+  });
+  return matchingObjs;
+}
+
